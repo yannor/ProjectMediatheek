620c13e5c37cf0918f0031f27b2ec917233fb043
yannor <yannick.servranckx@hotmail.com>
2015-02-12 11:48:06 +0100

    Start het project

---
+++ src/start/Main.java
@@ -0,0 +1,20 @@
+package start;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application 
+{
+
+    @Override
+    public void start(Stage stage) 
+    {
+
+    }
+
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+
+}
