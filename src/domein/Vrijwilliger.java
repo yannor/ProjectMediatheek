25d1637edb84b5b6eeb78d979cf3a3aa84d79dd1
Robin <Robin@Robin-PC>
2015-02-14 13:03:36 +0100

    Lege methoden uitleningen toegevoegd bij Medewerker en Vrijwilliger

---
+++ src/domein/Vrijwilliger.java
@@ -0,0 +1,36 @@
+
+package domein;
+
+public class Vrijwilliger extends Gebruiker 
+{
+
+    public Vrijwilliger() {
+        super();
+    }
+    
+    //methodes uitleningen
+    public void zoekUitlening()
+    {
+
+    }
+
+    public void zoekUitleningen()
+    {
+
+    }
+
+    public void voegUitleningToe()
+    {
+
+    }
+
+    public void wijzigUitlening()
+    {
+
+    }
+
+    public void verwijderUitlening()
+    {
+
+    }
+}
