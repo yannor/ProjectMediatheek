3be24aa6b0e4104529b28c7de38f7791de63ac02
yannor <yannick.servranckx@hotmail.com>
2015-02-19 10:34:42 +0100

    start met gebruikers en items

---
+++ src/uitlening/Uitlening.java
@@ -0,0 +1,67 @@
+package uitlening;
+
+import gebruikers.Gebruiker;
+import item.Item;
+import java.util.Date;
+
+public class Uitlening 
+{
+    private int id; //auto-nummeren
+    private int item;
+    private int gebruiker;
+    private Date datumUitgeleend;
+    
+
+    public Uitlening(Item item, Gebruiker gebruiker, Date datumUitgeleend) 
+    {
+        this.item=item.getId();
+        this.gebruiker=gebruiker.getId();
+        
+        this.datumUitgeleend = datumUitgeleend;
+       
+    }
+
+    public int getId() 
+    {
+        return id;
+    }
+
+    public void setId(int id) 
+    {
+        this.id = id;
+    }
+    
+    
+    public int getItem() 
+    {
+        return item;
+    }
+
+    public void setItem(int item) 
+    {
+        this.item = item;
+    }
+
+    public int getGebruiker() 
+    {
+        return gebruiker;
+    }
+
+    public void setGebruiker(int gebruiker) 
+    {
+        this.gebruiker = gebruiker;
+    }
+
+    public Date getDatumUitgeleend() 
+    {
+        return datumUitgeleend;
+    }
+
+    public void setDatumUitgeleend(Date datumUitgeleend) 
+    {
+        this.datumUitgeleend = datumUitgeleend;
+    }
+
+    
+}
+
